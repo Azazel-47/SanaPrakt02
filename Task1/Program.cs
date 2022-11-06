e5ab6cc8afe46206393dd79be95346e40a6f8fbb
Nikita Sehedin <zipz221_sno@student.ztu.edu.ua>
2022-11-06 14:44:34 +0100

    Lines change
    I changed last to lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.WriteLine("123");
 Console.WriteLine("456");
 Console.WriteLine("789");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("hhh");
+Console.WriteLine("fff");
+
